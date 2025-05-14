bcb5c2494a6badd140ce87c1da54f50dbe7a226e
Joaquin Pacis <34922795+SkibidiPaPaP@users.noreply.github.com>
2025-05-14 20:01:32 +0800

    Added GMaps Feature
    File, Variable renaming and new Gmaps Feature to be fixed

--- app/src/main/java/com/example/proxiesr_rfr/ui/dataInsight/DataInsightFragment.kt
+++ app/src/main/java/com/example/proxiesr_rfr/ui/dataInsight/DataInsightFragment.kt
@@ -1,4 +1,4 @@
-package com.example.proxiesr_rfr.ui.slideshow
+package com.example.proxiesr_rfr.ui.dataInsight
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.proxiesr_rfr.databinding.FragmentSlideshowBinding
+import com.example.proxiesr_rfr.databinding.FragmentGalleryBinding
 
-class SlideshowFragment : Fragment() {
+class DataInsightFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentGalleryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val dataInsightModel =
+            ViewModelProvider(this).get(DataInsightModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textGallery
+        dataInsightModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/proxiesr_rfr/ui/dataInsight/DataInsightModel.kt
+++ app/src/main/java/com/example/proxiesr_rfr/ui/dataInsight/DataInsightModel.kt
@@ -1,13 +1,13 @@
-package com.example.proxiesr_rfr.ui.home
+package com.example.proxiesr_rfr.ui.dataInsight
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class DataInsightModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is gallery Fragment"
     }
     val text: LiveData<String> = _text
 }
